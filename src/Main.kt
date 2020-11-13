40289d69cd389c74db403205c88b8352c00add6d
Vladead <vladead8@gmail.com>
2020-11-13 21:08:53 +0300

    Implementation of addAll()

--- src/Main.kt
+++ src/Main.kt
@@ -21,4 +21,15 @@ fun main(args: Array<String>) {
                 "Сумма площадей: ${list.sumByDouble { it -> it.calcArea() }}"
     )
 
+    val shapeAccumulator = ShapeAccumulator()
+
+    val listOfRectangle = arrayListOf(Rectangle(6.3, 3.2))
+    val listOfCircle = arrayListOf(Circle(3.4))
+    val square = Square(7.7)
+    val triangle = Triangle(5.0, 6.0, 7.0)
+
+    shapeAccumulator.add(square)
+    shapeAccumulator.add(triangle)
+    shapeAccumulator.addAll(listOfRectangle)
+    shapeAccumulator.addAll(listOfCircle)
 }
--- src/ShapeAccumulator.kt
+++ src/ShapeAccumulator.kt
@@ -1,17 +1,23 @@
-class ShapeAccumulator() {
-    private val shapes : MutableList<Shape> = ArrayList()
+class ShapeAccumulator {
+    private val shapes: MutableList<Shape> = ArrayList()
 
-    fun add (shape: Shape) {shapes.add(shape)}
+    fun add(shape: Shape) {
+        shapes.add(shape)
+    }
 
-    fun getTotalArea() : Double = shapes.sumByDouble { i -> i.calcArea() }
+    fun addAll(collection: Collection<Shape>) {
+        shapes.addAll(collection)
+    }
 
-    fun getTotalPerimeter() : Double = shapes.sumByDouble { i -> i.calcPerimeter() }
+    fun getTotalArea(): Double = shapes.sumByDouble { i -> i.calcArea() }
 
-    fun getMinAreaShape() : Shape? = shapes.minByOrNull { i -> i.calcArea() }
+    fun getTotalPerimeter(): Double = shapes.sumByDouble { i -> i.calcPerimeter() }
 
-    fun getMaxAreaShape() : Shape? = shapes.maxByOrNull { i -> i.calcArea() }
+    fun getMinAreaShape(): Shape? = shapes.minByOrNull { i -> i.calcArea() }
 
-    fun getMaxPerimeterShape() : Shape? = shapes.minByOrNull { i -> i.calcPerimeter() }
+    fun getMaxAreaShape(): Shape? = shapes.maxByOrNull { i -> i.calcArea() }
 
-    fun getMinPerimeterShape() : Shape? = shapes.minByOrNull { i -> i.calcPerimeter() }
+    fun getMaxPerimeterShape(): Shape? = shapes.maxByOrNull { i -> i.calcPerimeter() }
+
+    fun getMinPerimeterShape(): Shape? = shapes.minByOrNull { i -> i.calcPerimeter() }
 }
